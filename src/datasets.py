784d56d2aa4cfcfc474cd85497937e0d1c507aa3
Akama-Haruka <akamaharukalove@gmail.com>
2024-07-14 21:09:06 +0900

    change

--- src/datasets.py
+++ src/datasets.py
@@ -3,26 +3,7 @@ import numpy as np
 import torch
 from glob import glob
 
-train_set = ThingsMEGDataset(split='train', data_dir='/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)')
-print(f"Number of samples in train_set: {len(train_set)}")
-
-
-
-data_dir = '/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)'
-train_files = glob(os.path.join(data_dir, "train_X", "*.pt"))
-print(f"Number of train files: {len(train_files)}")
-
-loader_args = {
-    'batch_size': 64,
-    'num_workers': 2,
-    # 他の引数があれば追加
-}
-
-train_set = ThingsMEGDataset(split='train', data_dir='/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)')
-train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
-
-if len(train_files) == 0:
-    raise ValueError("No training data found. Please check the data directory and files.")
+t
 
 
 class ThingsMEGDataset(torch.utils.data.Dataset):
@@ -60,3 +41,24 @@ class ThingsMEGDataset(torch.utils.data.Dataset):
     @property
     def seq_len(self) -> int:
         return torch.load(os.path.join(self.data_dir, f"{self.split}_X", "00000.pt")).shape[1]
+
+rain_set = ThingsMEGDataset(split='train', data_dir='/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)')
+print(f"Number of samples in train_set: {len(train_set)}")
+
+
+
+data_dir = '/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)'
+train_files = glob(os.path.join(data_dir, "train_X", "*.pt"))
+print(f"Number of train files: {len(train_files)}")
+
+loader_args = {
+    'batch_size': 64,
+    'num_workers': 2,
+    # 他の引数があれば追加
+}
+
+train_set = ThingsMEGDataset(split='train', data_dir='/content/drive/.shortcut-targets-by-id/1qxAFAKovUXm28TMQUFxKljs01WcTHCeY/ColabData/dl_lecture_competition_pub/data(1)')
+train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
+
+if len(train_files) == 0:
+    raise ValueError("No training data found. Please check the data directory and files.")
