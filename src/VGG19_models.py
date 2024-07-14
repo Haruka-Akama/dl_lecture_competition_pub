bce4d6bba8f08453085f2d2ee1451818e744cbc2
Akama-Haruka <akamaharukalove@gmail.com>
2024-07-14 22:56:48 +0900

    VGG 2nd try

--- src/VGG19_models.py
+++ src/VGG19_models.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import models
+import torchvision.models as models
 
 class VGG19Classifier(nn.Module):
     def __init__(self, num_classes: int):
         super(VGG19Classifier, self).__init__()
         self.vgg19 = models.vgg19_bn(pretrained=True)
+        
+        # 最初の畳み込み層を再定義
+        self.vgg19.features[0] = nn.Conv2d(256, 64, kernel_size=3, padding=1)
         
         # 最後の全結合層を変更して、クラス数に適応
         self.vgg19.classifier[6] = nn.Linear(4096, num_classes)
