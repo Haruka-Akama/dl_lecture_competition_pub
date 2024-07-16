afdd8a7fe05b51b35a58a49866978ee7c9cde0c2
Akama-Haruka <akamaharukalove@gmail.com>
2024-07-16 11:16:41 +0900

    change

--- MEG_main.py
+++ MEG_main.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append('/workspace/dl_lecture_competition_pub/src')import os
+sys.path.append('/workspace/dl_lecture_competition_pub/src')
+
 import numpy as np
 import torch
 import torch.nn.functional as F
