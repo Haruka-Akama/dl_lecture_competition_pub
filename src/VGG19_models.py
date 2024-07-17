69133e0bea962cc3606555699ea3033bb17f263a
Haruka-Akama <akamaharukalove@gmail.com>
2024-07-18 00:56:21 +0900

    osekilab VGG19 2nd try successfully

--- src/VGG19_models.py
+++ src/VGG19_models.py
@@ -1,31 +1,37 @@
-from functools import partial
-from typing import Any, cast, Dict, List, Optional, Union
-
 import torch
 import torch.nn as nn
+import torchvision.models as models
 
-# 他のインポート部分が省略されていますが、ここに必要なモジュールをインポートしてください
+class fclip(nn.Module):
+    def __init__(self, num_input_channels, num_output_channels, num_subjects, num_classes):
+        super(fclip, self).__init__()
+        self.spatial_attention = SpatialAttentionLayer(num_input_channels, 270)
+        self.subject_specific = SubjectSpecificLayer(270, num_subjects)
+        self.residual_blocks = nn.ModuleList([ResidualDilatedConvBlock(270, 320) for _ in range(5)])
+        self.conv_final1 = nn.Conv1d(320, 640, kernel_size=1)
+        self.gelu = nn.GELU()
+        self.conv_final2 = nn.Conv1d(640, num_output_channels, kernel_size=1)
+        self.classifier = nn.Linear(num_output_channels, num_classes)
 
-class VGG19_BN_Weights(WeightsEnum):
-    IMAGENET1K_V1 = Weights(
-        url="https://download.pytorch.org/models/vgg19_bn-c79401a0.pth",
-        transforms=partial(ImageClassification, crop_size=224),
-        meta={
-            # _COMMON_METAは適切に定義されていると仮定しています
-            **_COMMON_META,
-            "num_params": 143678248,
-            "_metrics": {
-                "ImageNet-1K": {
-                    "acc@1": 74.218,
-                    "acc@5": 91.842,
-                }
-            },
-            "_ops": 19.632,
-            "_file_size": 548.143,
-        },
-    )
-    DEFAULT = IMAGENET1K_V1
+    def forward(self, X, subject_ids, sensor_locs):
+        X = self.spatial_attention(X, sensor_locs)
+        X = self.subject_specific(X, subject_ids)
+        for block in self.residual_blocks:
+            X = block(X)
+        X = self.conv_final1(X)
+        X = self.gelu(X)
+        X = self.conv_final2(X)
+        X = torch.mean(X, dim=-1)  # グローバル平均プーリング
+        X = self.classifier(X)
+        return X
 
-def vgg19_bn(*, weights: Optional[VGG19_BN_Weights] = None, progress: bool = True, **kwargs: Any) -> VGG:
-    weights = VGG19_BN_Weights.verify(weights)
-    return _vgg("E", True, weights, progress, **kwargs)
+# VGG19 のプリトレーニングされたモデルを使用する
+def get_vgg19_model(num_classes, pretrained=True):
+    if pretrained:
+        model = models.vgg19_bn(pretrained=True)
+    else:
+        model = models.vgg19_bn()
+    
+    # 最後の分類層を置き換える
+    model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes)
+    return model
