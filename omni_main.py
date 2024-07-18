097c993be91c82bf791a7dcb3d62666692ef075c
Haruka-Akama <akamaharukalove@gmail.com>
2024-07-18 11:39:14 +0900

    osekilab omni 1st try successfully

---
+++ omni_main.py
@@ -0,0 +1,131 @@
+import os, sys
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torchmetrics import Accuracy
+import hydra
+from omegaconf import DictConfig
+import wandb
+from termcolor import cprint
+import torch.nn as nn
+import torch.utils.data
+import collections.abc
+import collections
+
+# collections.Containerが存在しない場合の対策
+if not hasattr(collections, 'Container'):
+    collections.Container = collections.abc.Container
+
+from src.omni_datasets import ThingsMEGDataset
+from src.omni_models import BasicConvClassifier
+from src.omni_utils import set_seed
+
+@hydra.main(version_base=None, config_path="configs", config_name="omni_config")
+def run(args: DictConfig):
+    set_seed(args.seed)
+    logdir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    
+    if args.use_wandb:
+        wandb.init(mode="online", dir=logdir, project="MEG-classification")
+
+    # ------------------
+    #    Dataloader
+    # ------------------
+    loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
+    
+    train_set = ThingsMEGDataset("train", args.data_dir)
+    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
+    val_set = ThingsMEGDataset("val", args.data_dir)
+    val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
+    test_set = ThingsMEGDataset("test", args.data_dir)
+    test_loader = torch.utils.data.DataLoader(
+        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
+    )
+
+    # ------------------
+    #       Model
+    # ------------------
+    model = BasicConvClassifier(
+        train_set.num_classes, train_set.seq_len, train_set.num_channels)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device_ids = [0, 1]
+
+    # DataParallelを使用してモデルを複数のGPUで実行
+    model = nn.DataParallel(model, device_ids=device_ids)
+
+    # モデルを指定したデバイスに転送
+    model = model.to(device)
+
+    # 使用しているGPUを表示
+    print(f"Using GPUs: {device_ids}")
+    # ------------------
+    #     Optimizer
+    # ------------------
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    # ------------------
+    #   Start training
+    # ------------------  
+    max_val_acc = 0
+    accuracy = Accuracy(
+        task="multiclass", num_classes=train_set.num_classes, top_k=10
+    ).to(device)
+      
+    for epoch in range(args.epochs):
+        print(f"Epoch {epoch+1}/{args.epochs}")
+        
+        train_loss, train_acc, val_loss, val_acc = [], [], [], []
+        
+        model.train()
+        for X, y, subject_idxs in train_loader:
+            X, y = X.to(device), y.to(device)
+
+            y_pred = model(X)
+            
+            loss = F.cross_entropy(y_pred, y)
+            train_loss.append(loss.item())
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            acc = accuracy(y_pred, y)
+            train_acc.append(acc.item())
+
+        model.eval()
+        for X, y, subject_idxs in val_loader:
+            X, y = X.to(device), y.to(device)
+            
+            with torch.no_grad():
+                y_pred = model(X)
+            
+            val_loss.append(F.cross_entropy(y_pred, y).item())
+            val_acc.append(accuracy(y_pred, y).item())
+
+        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}")
+        torch.save(model.state_dict(), os.path.join(logdir, "model_last.pt"))
+        if args.use_wandb:
+            wandb.log({"train_loss": np.mean(train_loss), "train_acc": np.mean(train_acc), "val_loss": np.mean(val_loss), "val_acc": np.mean(val_acc)})
+        
+        if np.mean(val_acc) > max_val_acc:
+            cprint("New best.", "cyan")
+            torch.save(model.state_dict(), os.path.join(logdir, "model_best.pt"))
+            max_val_acc = np.mean(val_acc)
+            
+    # ----------------------------------
+    #  Start evaluation with best model
+    # ----------------------------------
+    model.load_state_dict(torch.load(os.path.join(logdir, "model_best.pt"), map_location=args.device))
+
+    preds = [] 
+    model.eval()
+    for X, subject_idxs in test_loader:        
+        preds.append(model(X.to(args.device)).detach().cpu())
+        
+    preds = torch.cat(preds, dim=0).numpy()
+    np.save(os.path.join(logdir, "submission"), preds)
+    cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")
+
+
+if __name__ == "__main__":
+    run()
